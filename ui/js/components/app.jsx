7082061bb4301dbf11574a550fbf51f42d5f41bd
Ellisande <justin.dragos@gmail.com>
2020-02-02 00:09:21 -0700

    Change main page styling to take up the entire view area

--- ui/js/components/app.jsx
+++ ui/js/components/app.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import { browserHistory, Link} from 'react-router';
-import {connectToMeetings, disconnectFromMeetings} from '../services/socket';
-import {filterMeetings} from '../../../shared/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { browserHistory, Link } from "react-router";
+import { connectToMeetings, disconnectFromMeetings } from "../services/socket";
+import { filterMeetings } from "../../../shared/actions";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.createMeeting = this.createMeeting.bind(this);
     this.updateMeetingName = this.updateMeetingName.bind(this);
@@ -15,62 +15,85 @@ class App extends Component {
       newMeetingName: undefined
     };
   }
-  componentWillMount(){
+  componentWillMount() {
     return connectToMeetings();
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     return disconnectFromMeetings();
   }
-  createMeeting(e){
+  createMeeting(e) {
     e.preventDefault();
     return browserHistory.push(`/meeting/${this.state.newMeetingName}`);
   }
-  updateMeetingName(e){
+  updateMeetingName(e) {
     const newMeetingName = e.target.value;
     const filterAction = filterMeetings(newMeetingName);
     this.props.dispatch(filterAction);
-    const newState = Object.assign({}, this.state, {newMeetingName});
+    const newState = Object.assign({}, this.state, { newMeetingName });
     return this.setState(newState);
   }
-  goToMeetings(){
-    this.setState(Object.assign({}, this.state, {active: undefined}));
-    return browserHistory.push('/');
+  goToMeetings() {
+    this.setState(Object.assign({}, this.state, { active: undefined }));
+    return browserHistory.push("/");
   }
-  setActive(activeTab){
-    return this.setState(Object.assign({}, this.state, {active: activeTab}));
+  setActive(activeTab) {
+    return this.setState(Object.assign({}, this.state, { active: activeTab }));
   }
-  render(){
-    const isActive = tabName => this.state.active === tabName ? 'active' : '';
+  render() {
+    const isActive = tabName => (this.state.active === tabName ? "active" : "");
     return (
-      <main className='home'>
-        <section className='light-box'>
-          <div className='upper-box'>
-            <div className='logo'>
-            </div>
-            <div className='title'>Note & Vote</div>
-            <div className='description-list'>
-              <Link to='/how' className={isActive('how')} onClick={this.setActive.bind(this, 'how')}>How</Link>
-              <Link to='/when' className={isActive('when')} onClick={this.setActive.bind(this, 'when')}>When</Link>
-              <Link to='/safety' className={isActive('safety')} onClick={this.setActive.bind(this, 'safety')}>Safety</Link>
-              <Link to='/new' className={`${isActive('new')} new`} onClick={this.setActive.bind(this, 'new')}>Lock & Knock</Link>
+      <main className="home">
+        <section className="home-container">
+          <div className="upper-box">
+            <div className="logo"></div>
+            <div className="title">Note & Vote</div>
+            <div className="description-list">
+              <Link
+                to="/how"
+                className={isActive("how")}
+                onClick={this.setActive.bind(this, "how")}
+              >
+                How
+              </Link>
+              <Link
+                to="/when"
+                className={isActive("when")}
+                onClick={this.setActive.bind(this, "when")}
+              >
+                When
+              </Link>
+              <Link
+                to="/safety"
+                className={isActive("safety")}
+                onClick={this.setActive.bind(this, "safety")}
+              >
+                Safety
+              </Link>
+              <Link
+                to="/new"
+                className={`${isActive("new")} new`}
+                onClick={this.setActive.bind(this, "new")}
+              >
+                Lock & Knock
+              </Link>
             </div>
           </div>
-          <div className='middle-box'>
+          <div className="middle-box">
             <form onSubmit={this.createMeeting}>
               <input
-                className='bubble-text'
-                placeholder='Meeting Name'
+                className="bubble-text"
+                placeholder="Meeting Name"
                 autoFocus
                 value={this.state.newMeetingName}
                 onChange={this.updateMeetingName}
-                onFocus={this.goToMeetings}/>
+                onFocus={this.goToMeetings}
+              />
             </form>
           </div>
-          <div className='lower-box'>
-            {this.props.children}
-          </div>
+          <div className="lower-box">{this.props.children}</div>
         </section>
-      </main>);
+      </main>
+    );
   }
 }
 
