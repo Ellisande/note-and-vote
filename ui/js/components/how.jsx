23ada92487dde2ea131944cfcc461d02c853765c
Ellisande <justin.dragos@gmail.com>
2020-02-02 00:48:55 -0700

    Reformat

--- ui/js/components/how.jsx
+++ ui/js/components/how.jsx
@@ -1,21 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class How extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
-      <div className='how'>
-        <div className='main-subtitle'><h2>How it Works</h2></div>
-        <div className='how-steps'>
-          <div className='how-step'>1. Post Your Ideas</div>
-          <div className='how-step'>2. Removed Duplicates</div>
-          <div className='how-step'>3. Vote for the Best</div>
-          <div className='how-step'>4. Discuss in Order</div>
-          <div className='how-step'>5. Stop When You Run Out of Time</div>
+      <div className="how">
+        <div className="main-subtitle">
+          <h2>How it Works</h2>
         </div>
-      </div>);
+        <div className="how-steps">
+          <div className="how-step">1. Post Your Ideas</div>
+          <div className="how-step">2. Remove Duplicates</div>
+          <div className="how-step">3. Vote for the Best</div>
+          <div className="how-step">4. Discuss in Order</div>
+          <div className="how-step">5. Stop When You Run Out of Time</div>
+        </div>
+      </div>
+    );
   }
 }
 
